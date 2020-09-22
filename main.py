77322099721bf93b50cb9fcde9d6bb89064658c1
niklas <niklas.frondorf@web.de>
2020-09-22 16:35:39 +0200

    added makemove() and checkmove()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random as rnd
+import os #os.system('cls')
 
 rows = 8;
 cols = 8;
@@ -19,10 +19,44 @@ board = [[start for i in range(rows)] for j in range(cols)]
 
 
 def main():
-    setchar(5, 1, 'x')
-    printboard();
-    print(getchar(5, 1))
-    # print(cross, tleft, tright, tup, tdown, hline, vline, urcorner,ulcorner,dlcorner,drcorner)
+    #test()
+    while True:
+        string = input("\nInput a number (0-7): ")
+        try:
+            num = int(string)
+            if (0 <= num and num <= 7):
+                print("yes")
+            else:
+                print("out of bounce")
+        except ValueError:
+            print(f"'{string}' isn't a number")
+
+
+def test():
+    setchar(4, 7, 'x')
+    setchar(4, 6, 'x')
+    setchar(4, 5, 'x')
+    setchar(4, 4, 'x')
+    setchar(4, 3, 'x')
+    setchar(4, 2, 'x')
+    setchar(4, 1, 'x')
+    #setchar(4, 0, 'x')
+    print(checkmove(4))
+
+
+def makemove(num: int, player: int):
+    for i in range(rows):
+        if(getchar(num, i) == ' '):
+            if(player == 1):
+                setchar(num, i, 'A')
+            elif(player == 2):
+                setchar(num, i, 'B')
+
+
+def checkmove(num: int):
+    if (getchar(num, 0) == ' '):
+        return True
+    return False
 
 
 def setchar(x, y, content):
